247a0475d3e7ec46705de46b2c7437a5653bab2c
ABS-Sayem <abssayem121194@gmail.com>
2023-07-31 15:20:20 +0600

    added preprocessInput module

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,10 @@
+import nltk
+import re
+import string
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+
 def cleanText(text):
-    #import nltk
-    import re
-    #import string
 
     #text = str(text).lower()
     #text = re.sub('\s', '', text)
@@ -9,10 +12,17 @@ def cleanText(text):
     #text = re.sub('https?://\S+|www\.\S+', '', text)
     #text = re.sub('<.*?>+', '', text)
     #text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
-    text = re.sub('\t', '', text)
+    text = re.sub('\t', ' ', text)
     text = re.sub('\n', ' ', text)
     #text = re.sub("\(cid:\d{1,2}\)", "", text)
     #text = re.sub('\W', ' ', text)
     #text = re.sub(' {2}', ' ', text)
 
     return(text)
+
+def preprocessInput(string):
+    u_input = str(string).lower()
+    u_input = re.sub('\t', ' ', u_input)
+    u_input = re.sub('\n', ' ', u_input)
+    u_input = ["".join(word) for word in u_input.split(' ') if word not in stop_words]
+    return(u_input)
